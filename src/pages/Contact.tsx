8d878742f64e653323779419142aab3c1fe864a2
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2025-05-01 12:05:20 +0300

    finalize on the contact us page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,92 @@
+'use client'
+
+import { ChevronRight } from "lucide-react"
 import { Started } from "../components/shared/Started"
 
 export const Contact = () => {
+
   return (
     <>
+      <div className="isolate bg-white px-6 py-10 sm:py-15 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        >
+        </div>
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact Us</h2>
+            <p className="mt-2 text-lg/8 text-gray-600">Feel free to reach out to us with any questions or concerns you may have.</p>
+        </div>
+        <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="flex w-1/4 items-center justify-between rounded-md bg-text-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-btn-primary"
+            >
+              <span>Send</span>
+              <ChevronRight className="h-4 w-4" />
+            </button>
+          </div>
+        </form>
+      </div>
       <Started />
     </>
   )
