3e0887e9cd5cb7cb5cfe5c55e6ba80381f78b7ce
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2025-05-08 10:26:54 +0300

    Add sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,49 +1,53 @@
-import { Dices, LayoutDashboard, LogOut, Settings, Ticket, User, Users } from "lucide-react";
+import { Dices, LayoutDashboard, LogOut, Settings, Ticket, User, Users, Menu, X } from "lucide-react";
+import { useState } from "react";
+
+const sidebarItems = [
+    { icon: LayoutDashboard, label: "Dashboard", href: "#dashboard" },
+    { icon: LayoutDashboard, label: "Category", href: "#category" },
+    { icon: Ticket, label: "Tickets", href: "#tickets" },
+    { icon: Dices, label: "Live Raffles", href: "#liveraffles" },
+    { icon: LayoutDashboard, label: "Completed Raffles", href: "#completedraffles" },
+    { icon: Users, label: "Users", href: "#users" },
+    { icon: Settings, label: "Settings", href: "#settings" },
+    { icon: User, label: "Profile", href: "#profile" },
+    { icon: LogOut, label: "Logout", href: "#logout" },
+];
 
 export const Sidebar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
         <>
-            <div className="w-64 h-screen bg-blue-900 p-5 shadow-md">
-                <h2 className="text-xl font-bold mb-4 text-white">Sidebar</h2>
+            {/* Toggle Button */}
+            <button
+                className={`fixed top-3 left-3 z-50 p-2 rounded md:hidden transition-colors duration-300 ${isSidebarOpen ? "bg-white text-blue-900" : "bg-blue-900 text-white"}`}
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            >
+                {isSidebarOpen ? <X size={24} className="text-bold"/> : <Menu size={24} />}
+            </button>
+
+            {/* Sidebar */}
+            <div
+                className={`fixed top-0 left-0 h-screen bg-blue-900 px-6 py-6 shadow-md transform transition-transform duration-300 ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+                    } md:translate-x-0 md:relative w-64`}
+            >
+                <h2 className="text-xl font-bold mb-4 text-white">Raffleit</h2>
                 <ul className="list-none">
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <LayoutDashboard className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Dashboard</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <LayoutDashboard className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Category</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <Ticket className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Tickets</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <Dices className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Live Raffles</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <LayoutDashboard className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Completed Raffles</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <Users className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#dashboard" className="text-white no-underline hover:text-blue-500">Users</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <Settings className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#settings" className="text-white no-underline hover:text-blue-500">Settings</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <User className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#profile" className="text-white no-underline hover:text-blue-500">Profile</a>
-                    </li>
-                    <li className="mb-2 mt-4 gap-2 flex items-center group">
-                        <LogOut className="mr-2 text-white group-hover:scale-110 group-hover:text-blue-500 transition-transform duration-200" size={18}/>
-                        <a href="#logout" className="text-white no-underline hover:text-blue-500">Logout</a>
-                    </li>
+                    {sidebarItems.map(({ icon: Icon, label, href }, index) => (
+                        <li
+                            key={index}
+                            className="mb-2 mt-4 gap-2 flex items-center hover:scale-110 hover:text-blue-500 transition-transform duration-200 group"
+                        >
+                            <Icon className="text-white" size={18} />
+                            <a href={href} className="flex items-center">
+                                <span className="md:inline-block ml-2 text-white group-hover:text-blue-500">
+                                    {label}
+                                </span>
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
-    )
+    );
 };
--- src/pages/dashboard/AdminDashboard.tsx
+++ src/pages/dashboard/AdminDashboard.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../../components/dashboard/Sidebar"
 export const AdminDashboard = () => {
     return (
         <>
-            <div className="flex min-h-screen bg-gray-100">
+            <div className="flex min-h-screen h-screen md:h-screen bg-gray-100">
                 <Sidebar />
                 <div className="flex-1 p-6">
                     <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
